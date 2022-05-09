7b4010c2c773fc2ce250ee2961060914d1f11fde
Pedro Luis <pepluisgon01@gmail.com>
2022-05-09 08:53:31 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/dsmovie2/Demodsmovie2Application.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmovie2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demodsmovie2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demodsmovie2Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dsmovie2/Demodsmovie2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmovie2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demodsmovie2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
